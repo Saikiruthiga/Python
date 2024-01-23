a2b1dd824d9b4786e71901b519cb1a773a7d7cd3
Saikiruthiga <Saikiruthiga06@gmail.com>
2024-01-23 12:05:29 +0100

    Python Programs

---
+++ Odd_Even.py
@@ -0,0 +1,6 @@
+print("Program for finding the given value is odd or even")
+number = input("Enter a value : ")
+if(float(number) %2 ==0):
+    print("The given number is an even number")
+else:
+    print("The given number is an odd number")
